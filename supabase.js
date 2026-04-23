cbe10060b0b27695af0e28300a4ef4861a337c95
heloisa-jpg <heloisa@grupoapostefacil.com>
2026-04-23 16:27:57 -0300

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || 'https://SEU_PROJETO.supabase.co'
-const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY || 'SUA_CHAVE_ANON'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
